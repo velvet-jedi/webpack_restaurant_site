bbadcc23ec05e1623492cc62c0c40f0c481c801e
velvet-jedi <pranjalruhela2811@gmail.com>
2024-02-17 17:37:04 +0530

    contact form

--- src/about.js
+++ src/about.js
@@ -4,5 +4,6 @@ import './style.css';
 export default function initializeAboutPage() {
     const aboutContentDiv = document.getElementById('about-content');
 
-    // Add your about page specific JavaScript code here
+    
+
 }
--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,51 @@
 // contact.js
 import './style.css';
 
-export default function initializeContactPage() {
-    const contactContentDiv = document.getElementById('contact-content');
+function initializeContactPage() {
+    const contactContentDiv = document.querySelector('.contact-content');
+
+    const containerDiv = document.createElement('div');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = "CONTACT US";
+    h1.style.color = 'yellow';
+    
+
+    const contactForm = `
+        <form>
+            <fieldset id="row1">
+                <label for="first-name"></label>
+                <input type="text" required id="first-name" placeholder="First name"/>
+                <label for="last-name"></label>
+                <input type="text" required id="last-name" placeholder="Last name"/>
+            </fieldset>
+
+            <fieldset id="row2">
+                <label for="phone"></label>
+                <input type="text" pattern="(7|8|9)\d{9}" required id="phone" placeholder="Phone number"/>
+                <label for="email"></label>
+                <input type="email" required id="email" placeholder="Email"/>
+            </fieldset>
+
+            <label for="description"></label>
+            <textarea type="text" required id="description" placeholder="Subject"></textarea>
+
+            <fieldset id="row-btn">
+                <button type="submit">Send</button>
+                <button type="reset">Reset form</button>
+            </fieldset>
+
+        </form>
+    `;
+
+    contactContentDiv.appendChild(h1);
+    containerDiv.innerHTML = contactForm;
+    contactContentDiv.appendChild(containerDiv);
 
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    initializeContactPage();
+})
+    
+    
